d4b20d51b6d6d0c86275e357dd1741ab92468d6a
Linda Castillor <linda@raidho.mx>
2017-10-31 17:06:27 -0600

    commit
    Added Account plan for, enterprise, personal plan, request and confirmation request

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,12 @@
-//CUSTOM SCROLL
-var windowWidth = $(window).width();
-if(windowWidth > 768){
-	$(".scroll-content").mCustomScrollbar({
-		theme:"dark"
-	});
-}
-
-//CUSTOM SCROLL WHITE VERSION
-var windowWidth = $(window).width();
-if(windowWidth > 768){
-	$(".scroll-content-white").mCustomScrollbar({
-		theme:"light-2"
-	});
-}
-
-// CIRCLE PROGRESS BAR: DRAW GREEN LINE
-$(".circle-progress-bar").each(function(){
-	var val = parseInt($('#number').text());
-	var $circle = $('#svg #bar');
-	if (isNaN(val)) {
-		val = 100;
-	}
-	else{
-		var r = $circle.attr('r');
-		var c = Math.PI*(r*2);
-
-		if (val < 0) { val = 0;}
-		if (val > 100) { val = 100;}
-
-		var pct = ((100-val)/100)*c;
-
-		$circle.css({ strokeDashoffset: pct});
-
-		$('#cont').attr('data-pct',val);
-	}
+//TABS
+$(".tabs-menu a").click(function(event) {
+	event.preventDefault();
+	$(this).parent().addClass("current");
+	$(this).parent().siblings().removeClass("current");
+	var tab = $(this).attr("href");
+	$(".tab-content").not(tab).css("display", "none");
+	$(tab).fadeIn();
 });
-
-
 
 // FIX FOOTER TO BOTTOM: ADD PADDING ON .MAIN-CONTENT
 var footerHeight = $('footer').outerHeight();
@@ -45,24 +15,6 @@ $(window).resize(function() {
 	var footerHeight = $('footer').outerHeight();
 	$('.main-content').css({'padding-bottom' : footerHeight});
 });
-
-// SHOW ASSITANT STATUS
-$('.trigger-assistant-status .question-mark-btn').click(function(){
-	$('.canvas-progress-assistant').addClass('show');
-	$('.canvas-grid').addClass('show');
-	$('.canvas-grid-v2').addClass('show');
-	$('.main-content-block').addClass('show');
-	$('.trigger-assistant-status').addClass('hide');
-});
-
-$('.canvas-progress-assistant .question-mark-btn').click(function(){
-	$('.canvas-progress-assistant').removeClass('show');
-	$('.canvas-grid').removeClass('show');
-	$('.canvas-grid-v2').removeClass('show');
-	$('.main-content-block').removeClass('show');
-	$('.trigger-assistant-status').removeClass('hide');
-});
-
 
 // TAGS
 $(function() {
